d50245c780a78460925ce896758f4443debc9bbb
Dan Hooper <dan.c.hooper@gmail.com>
2014-04-08 09:07:11 -0400

    Bug fix

--- src/pong_ratings/apps/score/models.py
+++ src/pong_ratings/apps/score/models.py
@@ -8,19 +8,19 @@ class Rating(models.Model):
     def __unicode__(self):
         return '%s - %s' % (self.user, self.score)
 
-    def calculate_game(self, score):
-        handicap = int(abs(self.score - score.score)/50)
-        if self.score < score.score:
-            if handicap <= 10:
-                self.handicap_self = handicap
-                self.handicap_other = 0
-            else:
-                self.handicap_other = 10
-                self.handicap_self = 10 - handicap
-        else:
+    def calculate_game(self, curr_users_score):
+        handicap = int(abs(self.score - curr_users_score.score)/50)
+        if self.score < curr_users_score.score:
             if handicap <= 10:
                 self.handicap_other = handicap
                 self.handicap_self = 0
             else:
                 self.handicap_other = 10
                 self.handicap_self = 10 - handicap
+        else:
+            if handicap <= 10:
+                self.handicap_self = handicap
+                self.handicap_other = 0
+            else:
+                self.handicap_self = 10
+                self.handicap_other = 10 - handicap
