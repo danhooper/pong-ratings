9158f3ab0076431ea647bb506c2ecb2799dfb26a
Dan Hooper <dan.c.hooper@gmail.com>
2014-04-08 09:01:16 -0400

    Bug fix

--- src/pong_ratings/apps/score/models.py
+++ src/pong_ratings/apps/score/models.py
@@ -10,7 +10,7 @@ class Rating(models.Model):
 
     def calculate_game(self, score):
         if self.score > score.score:
-            handicap = int((self.score - score.score))
+            handicap = int((self.score - score.score)/50)
             if handicap <= 10:
                 self.handicap_other = handicap
                 self.handicap_self = 0
@@ -18,7 +18,7 @@ class Rating(models.Model):
                 self.handicap_other = 10
                 self.handicap_self = 10 - handicap
         else:
-            handicap = int((score.score - self.score))
+            handicap = int((score.score - self.score)/50)
             if handicap <= 10:
                 self.handicap_self = handicap
                 self.handicap_other = 0
